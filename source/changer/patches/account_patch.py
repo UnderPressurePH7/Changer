484164b05909a9edd1641fb86f9759de0936f02b
UnderPressurePH7 <149047913+UnderPressurePH7@users.noreply.github.com>
2025-10-03 01:24:55 +0300

    _original_name = None

--- source/changer/patches/account_patch.py
+++ source/changer/patches/account_patch.py
@@ -1,7 +1,9 @@
+# source/changer/patches/account_patch.py
 import BigWorld
 from ..utils import print_log, print_debug, print_error
 
 _original_init = None
+_original_name = None
 _config = None
 
 def patch(config):
@@ -12,16 +14,18 @@ def patch(config):
         from Account import PlayerAccount
         _original_init = PlayerAccount.__init__
         PlayerAccount.__init__ = _patched_init
-        print_debug("Account patched successfully")
+        print_log("Account patched successfully")
     except Exception as e:
         print_error("Failed to patch Account: %s" % str(e))
 
 def _patched_init(self):
+    global _original_name
     _original_init(self)
     try:
-        original_name = self.name
+        if _original_name is None:
+            _original_name = self.name
         new_name = _config.load_nickname_from_config()
         self.name = new_name
-        print_debug("Changed nickname from '%s' to '%s'" % (original_name, new_name))
+        print_log("Nickname changed: %s -> %s" % (_original_name, new_name))
     except Exception as e:
         print_error("Error in patched Account.__init__: %s" % str(e))
